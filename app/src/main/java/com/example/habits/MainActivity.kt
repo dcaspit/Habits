c0deac5624ec252b336e20ca8bb028336d535206
Daniel Caspit <dcaspit@gmail.com>
2024-01-17 23:09:21 +0200

    added: light / dark mode toggle

--- app/src/main/java/com/example/habits/MainActivity.kt
+++ app/src/main/java/com/example/habits/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
--- app/src/main/java/com/example/habits/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/habits/fragments/home/HomeFragment.kt
@@ -1,13 +1,23 @@
 package com.example.habits.fragments.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.habits.R
 import com.example.habits.data.viewModels.HabitsViewModel
 import com.example.habits.databinding.FragmentHomeBinding
 import com.example.habits.fragments.home.components.HabitAddItem
@@ -37,6 +47,8 @@ class HomeFragment : Fragment() {
             binding.recyclerView.adapter = baseRecyclerAdapter
             binding.recyclerView.layoutManager = LinearLayoutManager(binding.root.context)
 
+            sharedPreferences = requireActivity().getSharedPreferences("MODE", Context.MODE_PRIVATE)
+
             mDatabaseViewModel.getAllHabits.observe(viewLifecycleOwner) {
                 val items: List<BaseItem> = it.map { habit -> HabitItem(habit) }
                 val updatedItems = items + HabitAddItem()
@@ -49,6 +61,39 @@ class HomeFragment : Fragment() {
             e.printStackTrace()
         }
         return null;
+    }
+
+    var sharedPreferences: SharedPreferences? = null
+    var editor: SharedPreferences.Editor? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.list_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when(menuItem.itemId) {
+                    R.id.menu_theme -> {
+                        val nightMode = sharedPreferences?.getBoolean("night", false)
+
+                        if(nightMode == true) {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                            editor = sharedPreferences?.edit()
+                            editor?.putBoolean("night", false)
+                        } else {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                            editor = sharedPreferences?.edit()
+                            editor?.putBoolean("night", true)
+                        }
+                        editor?.apply()
+                    }
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     override fun onDestroyView() {
