996e52a171308da744c790deef4741664fe0661a
Daniel Caspit <dcaspit@gmail.com>
2024-04-01 23:27:04 +0300

    Added calendar view to the app

--- client-android/app/src/main/java/com/example/habits/fragments/home/HomeFragment.kt
+++ client-android/app/src/main/java/com/example/habits/fragments/home/HomeFragment.kt
@@ -9,11 +9,14 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -26,9 +29,24 @@ import com.example.habits.R
 import com.example.habits.data.baseObjects.BaseRecyclerAdapter
 import com.example.habits.data.models.HabitData
 import com.example.habits.data.viewModels.DatabaseViewModel
+import com.example.habits.databinding.CalendarDayBinding
 import com.example.habits.databinding.FragmentHomeBinding
 import com.example.habits.fragments.home.components.HabitItem
 import com.google.android.material.snackbar.Snackbar
+import com.kizitonwose.calendar.core.Week
+import com.kizitonwose.calendar.core.WeekDay
+import com.kizitonwose.calendar.core.atStartOfMonth
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.yearMonth
+import com.kizitonwose.calendar.view.ViewContainer
+import com.kizitonwose.calendar.view.WeekDayBinder
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
 
 class HomeFragment : Fragment() {
 
@@ -40,6 +58,10 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
 
     private val baseRecyclerAdapter: BaseRecyclerAdapter by lazy { BaseRecyclerAdapter() }
+
+    private var selectedDate = LocalDate.now()
+
+    private val dateFormatter = DateTimeFormatter.ofPattern("dd")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -72,32 +94,83 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val menuHost: MenuHost = requireActivity()
-//        menuHost.addMenuProvider(object : MenuProvider {
-//            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-//                menuInflater.inflate(R.menu.list_fragment_menu, menu)
-//            }
-//
-//            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-//                when(menuItem.itemId) {
-//                    R.id.menu_theme -> {
-//                        val nightMode = sharedPreferences?.getBoolean("night", false)
-//
-//                        if(nightMode == true) {
-//                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-//                            editor = sharedPreferences?.edit()
-//                            editor?.putBoolean("night", false)
-//                        } else {
-//                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//                            editor = sharedPreferences?.edit()
-//                            editor?.putBoolean("night", true)
-//                        }
-//                        editor?.apply()
-//                    }
-//                }
-//                return true
-//            }
-//        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            val bind = CalendarDayBinding.bind(view)
+            lateinit var day: WeekDay
+
+            init {
+                view.setOnClickListener {
+                    if (selectedDate != day.date) {
+                        val oldDate = selectedDate
+                        selectedDate = day.date
+                        binding.exSevenCalendar.notifyDateChanged(day.date)
+                        oldDate?.let { binding.exSevenCalendar.notifyDateChanged(it) }
+                    }
+                }
+            }
+
+            fun bind(day: WeekDay) {
+                this.day = day
+                bind.exSevenDateText.text = dateFormatter.format(day.date)
+                bind.exSevenDayText.text = day.date.dayOfWeek.displayText()
+
+                val colorRes = if (day.date == selectedDate) {
+                    R.color.example_7_yellow
+                } else {
+                    R.color.example_7_white
+                }
+                bind.exSevenDateText.setTextColor(view.context.getColorCompat(colorRes))
+                bind.exSevenSelectedView.isVisible = day.date == selectedDate
+            }
+        }
+
+        binding.exSevenCalendar.dayBinder = object : WeekDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, data: WeekDay) = container.bind(data)
+        }
+
+        val currentMonth = YearMonth.now()
+        binding.exSevenCalendar.setup(
+            currentMonth.minusMonths(5).atStartOfMonth(),
+            currentMonth.plusMonths(5).atEndOfMonth(),
+            firstDayOfWeekFromLocale(),
+        )
+        binding.exSevenCalendar.scrollToDate(LocalDate.now())
+    }
+
+    fun Context.getColorCompat(@ColorRes color: Int) =
+        ContextCompat.getColor(this, color)
+
+    fun YearMonth.displayText(short: Boolean = false): String {
+        return "${this.month.displayText(short = short)} ${this.year}"
+    }
+
+    fun Month.displayText(short: Boolean = true): String {
+        val style = if (short) TextStyle.SHORT else TextStyle.FULL
+        return getDisplayName(style, Locale.ENGLISH)
+    }
+
+    fun DayOfWeek.displayText(uppercase: Boolean = false): String {
+        return getDisplayName(TextStyle.SHORT, Locale.ENGLISH).let { value ->
+            if (uppercase) value.uppercase(Locale.ENGLISH) else value
+        }
+    }
+
+
+    fun getWeekPageTitle(week: Week): String {
+        val firstDate = week.days.first().date
+        val lastDate = week.days.last().date
+        return when {
+            firstDate.yearMonth == lastDate.yearMonth -> {
+                firstDate.yearMonth.displayText()
+            }
+            firstDate.year == lastDate.year -> {
+                "${firstDate.month.displayText(short = false)} - ${lastDate.yearMonth.displayText()}"
+            }
+            else -> {
+                "${firstDate.yearMonth.displayText()} - ${lastDate.yearMonth.displayText()}"
+            }
+        }
     }
 
     private fun setupRecyclerView() {
